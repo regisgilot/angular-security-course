f8237903d9e7938b6e9df4743713aa0a39a369fd
rgilot <regis.gilot@axitam.eu>
2022-08-20 05:44:06 +0200

    config

--- server/password-validation.ts
+++ server/password-validation.ts
@@ -1,4 +1,3 @@
-
 import * as passwordValidator from 'password-validator';
 
 
@@ -7,15 +6,14 @@ const schema = new passwordValidator();
 
 // Add properties to it
 schema
-    .is().min(10)                                    // Minimum length 10
-    .has().uppercase()                              // Must have uppercase letters
-    .has().lowercase()                              // Must have lowercase letters
-    .has().digits()                                 // Must have digits
-    .has().not().spaces()                           // Should not have spaces
-    .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+  .is().min(10)                                    // Minimum length 10
+  .has().uppercase()                              // Must have uppercase letters
+  .has().lowercase()                              // Must have lowercase letters
+  .has().digits()                                 // Must have digits
+  .has().not().spaces()                           // Should not have spaces
+  .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
 
 
-
-export function validatePassword(password:string) {
-    return schema.validate(password, {list:true});
+export function validatePassword(password: string): any {
+  return schema.validate(password, {list: true});
 }
